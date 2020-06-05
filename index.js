0c028e99f3c34ead639a456624ef307e99bb4be2
srivsa01 <65112268+srivsa01@users.noreply.github.com>
2020-06-05 23:56:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Todays new World!");
+    response.end("Hello Year 2020 World!");
 });
 
 const port = process.env.PORT || 1337;
